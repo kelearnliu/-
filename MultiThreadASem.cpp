fd75f6a02f32f6a7ea76cc6af40cda59dba5e2bf
lgy <kelearnliu@163.com>
2023-08-24 11:35:57 +0800

    完成四种书写方法

--- MultiThreadASem.cpp
+++ MultiThreadASem.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <thread>
+#include <semaphore>
+#include <atomic>
+
+std::atomic<int> counter(0);
+// 这里报错  不清楚原因  但是能够执行
+std::counting_semaphore<1> sem(1);// 计数信号量，初始值为 1
+void increment()
+{
+    for(int i=0;i<500;i++)
+    {
+        sem.acquire();  // 等待信号量可用
+        ++counter;      // 自增操作
+        sem.release();  // 释放信号量
+        std::cout<<"thread_id:"<<std::this_thread::get_id()<<"\t";
+        std::cout<<"p_id:"<<static_cast<pid_t>(getpid())<<"\t";
+        std::cout<<"counter:"<<counter<<std::endl;
+    }
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout<<"git"<< std::endl;
+    std::thread thread1(increment);
+    std::thread thread2(increment);
+    std::cout<<"main:"<<getpid()<<"\t"<<"main thread:"<<std::this_thread::get_id()<<std::endl;
+    thread1.join();
+    thread2.join();
     return 0;
 }
--- MultiThreadConditionVariable.cpp
+++ MultiThreadConditionVariable.cpp
@@ -12,7 +12,6 @@ void increment()
     {
         std::unique_lock<std::mutex> lock(mtx);
         counter++;
-
         std::cout<<"thread_id:"<<std::this_thread::get_id()<<"\t";
         std::cout<<"p_id:"<<static_cast<pid_t>(getpid())<<"\t";
         std::cout<<"counter:"<<counter<<std::endl;
@@ -25,8 +24,10 @@ int main() {
 
     std::cout<<"main:"<<getpid()<<"\t"<<"main thread:"<<std::this_thread::get_id()<<std::endl;
     {
+        // lock 是一个已经锁住的互斥锁，cv.wait() 会在等待期间解锁这个互斥锁，允许其他线程访问被保护的资源。
         std::unique_lock<std::mutex> lock(mtx);
-        cv.wait(lock,[]{return counter==1000;});
+        // 后面返回的是true，函数返回不等待  返回的是false 释放锁 继续等待
+        cv.wait(lock,[](){return counter==1000;});
     }
     thread1.join();
     thread2.join();
